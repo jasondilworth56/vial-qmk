6805504c39c22486aef96200472d2fb84c26bb6b
Jason Dilworth <hello@jasondilworth.co.uk>
2026-03-08 09:16:35 +0000

    Initial work

--- keyboards/crkbd/rev4_1/standard/keymaps/vial/rules.mk
+++ keyboards/crkbd/rev4_1/standard/keymaps/vial/rules.mk
@@ -1,7 +1,11 @@
 VIA_ENABLE          = yes
 VIAL_ENABLE         = yes
-VIALRGB_ENABLE		= yes
 LTO_ENABLE          = yes
+
+QMK_SETTINGS        = yes
+
+RGB_MATRIX_ENABLE	= yes
+VIALRGB_ENABLE      = yes
 
 MOUSEKEY_ENABLE     = yes
 OLED_ENABLE         = no
@@ -9,8 +13,7 @@ OLED_DRIVER         = SSD1306
 EXTRAKEY_ENABLE     = yes
 COMBO_ENABLE        = yes
 
-QMK_SETTINGS        = yes
-
 CAPS_WORD_ENABLE = yes
 LAYER_LOCK_ENABLE = yes
 REPEAT_KEY_ENABLE = yes
+
